cf07f2250afaa8d45729d373fb9e749dcd8096fd
Kunal Sareen <u6509424@anu.edu.au>
2018-12-31 07:01:13 +1100

    Added functions for modularity

--- pos_tagger.py
+++ pos_tagger.py
@@ -2,32 +2,39 @@ import spacy
 import csv
 import sys
 
+# Load english language
+nlp = spacy.load('en_core_web_sm')
+
 # For pretty printing of tokenisation
 def print_table(doc):
-    header = '| {:8} | {:8} | {:8} | {:8} | {:10} | {:8} | {:8} | {:8} |'.format("Text", "Lemma", "PoS", "Tag", "Dependency", "Shape", "Alpha", "Stop")
+    header = '| {:10} | {:10} | {:10} | {:10} |'.format("Text", "PoS", "Tag", "Dependency")
     separator = '-' * len(header)
 
     print(header)
     print(separator)
     
     for token in doc:
-        print('| {:8} | {:8} | {:8} | {:8} | {:10} | {:8} | {:8} | {:8} |'.format(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop))
+        print('| {:10} | {:10} | {:10} | {:10} |'.format(token.text, token.pos_, token.tag_, token.dep_))
 
 # Write tagged.csv to the current directory
 def write_csv(doc):
     with open('tagged.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(csvfile, delimiter=' ', quotechar='|')
 
         for token in doc:
-            writer.writerow([token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop])
+            writer.writerow([token.text, token.pos_, token.tag_, token.dep_])
 
+# pos tagging for lists
+def pos_list(sentences):
+    for sentence in sentences:
+        doc = nlp(u'{}'.format(sentence))
+        write_csv(doc)
 
-# Load English as the language and get the text to be parsed
-nlp = spacy.load('en_core_web_sm')
-text = sys.argv[1]
+# pos tagging for csv with relative filepath 
+def pos_csv(filepath):
+    with open(filepath, 'r', newline='') as text:
+        reader = csv.reader(text, delimiter=';', quotechar='|')
+        for sentence in reader:
+            doc = nlp(u'{}'.format(sentence))
+            write_csv(doc)
 
-doc = nlp(u'{}'.format(text))
-#doc = nlp(u"Kotak Mahindra stocks fall due to bad quarter")
-
-write_csv(doc)
-#print_table(doc)
